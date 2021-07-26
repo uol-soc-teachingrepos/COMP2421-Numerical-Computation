4b15af235b3d0b835986f1130e751ac5a301b40b
Tom Ranner <T.Ranner@leeds.ac.uk>
2021-07-26 15:40:00 +0100

    try to make a better index page

--- pandoc/make-index.py
+++ pandoc/make-index.py
@@ -3,6 +3,25 @@ import os.path
 import pypandoc
 
 
+def dirname2content(dirname):
+    lecnumber = int(dirname[3:])
+    index = os.path.join(dirname, "index.html")
+    embed = os.path.join(dirname, "embed.html")
+
+    ret = f"## Lecture {lecnumber}\n\n"
+
+    ret += f"""
+<div style="position:relative;padding-top:66%;">
+<iframe src="{embed}" frameborder="0" allowfullscreen
+style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+</div>
+    """
+
+    ret += f'\n\n<a href="{index}">Full screen</a>\n'
+
+    return ret
+
+
 output = sys.argv[1]
 input = sorted(sys.argv[2:])
 
@@ -13,8 +32,8 @@ in_text = """
 
 for fn in input:
     lec = os.path.splitext(fn)[0]
-    html = os.path.join(lec, "index.html")
-    in_text += f"\n- [{lec}]({html})"
+    in_text += dirname2content(lec)
+    in_text += "\n\n"
 
 out_text = pypandoc.convert_text(in_text, "html", format="md", extra_args=["-s"])
 

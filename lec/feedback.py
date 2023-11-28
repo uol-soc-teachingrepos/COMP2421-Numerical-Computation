5525ebf8fb3d4786f384775139e1538c209439e9
Tom Ranner <T.Ranner@leeds.ac.uk>
2023-11-28 12:15:22 +0000

    updates to lec18 and feedback qrcode

---
+++ lec/feedback.py
@@ -0,0 +1,17 @@
+import qrcode
+from matplotlib import pyplot as plt
+
+
+def feedback():
+    FEEDBACK_URL = "https://go.blueja.io/BeQtMbSiPUm6DWQUJ2WVgA"
+    qr = qrcode.make(FEEDBACK_URL)
+
+    plt.title("Module feedback now open!")
+    plt.imshow(qr, cmap="Greys_r")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    feedback()
